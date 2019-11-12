24fcc37fab1ee261126631c80f1bbe154484a06b
Ian Laird <irlaird@gmail.com>
2019-11-11 21:20:31 -0700

    Updated setup to include subpackages

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
-with open('README.md') as f:
-    long_description = f.readline()
+with open('README.md', "r") as f:
+    long_description = f.read()
 
 setup(
     name='dnry_config',
@@ -15,9 +15,16 @@ setup(
     keywords=['config', 'configuration', 'dnry'],
     description='Multi-source config library',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     license='MIT',
-    packages=['dnry.config'],
+    packages=[
+        'dnry.config',
+        'dnry.config.arg',
+        'dnry.config.delegate',
+        'dnry.config.environ',
+        'dnry.config.in_memory',
+        'dnry.config.yaml',
+    ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
